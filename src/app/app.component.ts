63189d66862b12e279a10c531b2b804fa98e2445
Orea Jessica <86116@alumnos.utleon.edu.mx>
2025-09-12 18:50:46 -0600

    Primer Comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesListComponent } from "./heroes/heroes-list/heroes-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

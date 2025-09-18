e83ea09141825d029157f98d628140009ad41eab
Orea Jessica <86116@alumnos.utleon.edu.mx>
2025-09-17 19:48:45 -0600

    formulario

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -2,10 +2,48 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-list',
-  imports: [],
-  templateUrl: './heroes-list.component.html',
-  styleUrl: './heroes-list.component.css'
+  templateUrl: './heroes-list.component.html'
 })
+
 export class HeroesListComponent {
+
+  imageWidth:number = 40;
+  imageMargin:number=2;
+  showImage:boolean=true;
+
+  muestraImagen():void{
+    this.showImage=!this.showImage;}
+
+heroes: any[] = [
+  {
+    imagen: "https://dragonball-api.com/characters/goku_normal.webp",
+    nombre: "Goku",
+    descripcion: "Kame Hame Ha",
+    raza: "Saiyan",
+    ki: 9000
+  },
+  {
+    imagen: "https://dragonball-api.com/characters/vegeta_normal.webp",
+    nombre: "Vegeta",
+   descripcion: "Final Flash",
+    raza: "Saiyan",
+    ki: 8000
+  },
+
+{
+  imagen: "https://dragonball-api.com/characters/piccolo_normal.webp",
+  nombre: "Piccolo",
+  descripcion: "Makankosappo",
+  raza: "Namekiano",
+  ki: 7000
+},
+{
+  imagen: "https://dragonball-api.com/characters/Freezer.webp",
+  nombre: "Freezer",
+  descripcion: "Death Ball",
+  raza: "Alien",
+  ki: 9500
+}
+]
 
 }
